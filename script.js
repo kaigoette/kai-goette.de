3cffb7b8f169e9f88d2bd7e16352a1b6e4134b2e
Kai Goette <kai.goe@outlook.de>
2024-12-19 05:56:53 +0100

    first preview

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const footer = document.querySelector("footer");
-    const footerHeight = footer.offsetHeight;
-
-    document.documentElement.style.setProperty('--footer-height', `${footerHeight}px`);
-});
