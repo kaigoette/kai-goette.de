559e4abea7c42c29cbdaa4f7f746beb15e06c706
Kai Goette <kai.goe@outlook.de>
2024-12-19 06:08:49 +0100

    resize

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const viewportHeight = window.innerHeight;
-document.documentElement.style.setProperty('--vh', `${viewportHeight}px`);
+// Function to set the CSS variable
+function setVhVariable() {
+    const viewportHeight = window.innerHeight;
+    document.documentElement.style.setProperty('--vh', `${viewportHeight}px`);
+}
+
+// Set the variable on initial load
+setVhVariable();
+
+// Update the variable on window resize
+window.addEventListener('resize', setVhVariable);
